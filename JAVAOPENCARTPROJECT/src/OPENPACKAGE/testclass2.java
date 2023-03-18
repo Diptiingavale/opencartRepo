f9df90bf2ecc94aaa6a65405f722daa422893597
Diptiingavale <127374765+Diptiingavale@users.noreply.github.com>
2023-03-18 23:04:28 +0530

    myopencartprogram

---
+++ JAVAOPENCARTPROJECT/src/OPENPACKAGE/testclass2.java
@@ -0,0 +1,12 @@
+package OPENPACKAGE;
+
+public class testclass2 {
+
+	public static void main(String[] args) {
+		System.out.println(" pagelayer class");
+		System.out.println(" testNG");
+		System.out.println(" utility package");
+
+	}
+
+}
